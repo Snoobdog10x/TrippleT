53f2e146b5c967177124d23c37256d56792a56ed
Thanh Duy Nguyen <67373941+Snoobdog10x@users.noreply.github.com>
2021-05-09 19:59:48 +0700

    update

--- Admin/index.php
+++ Admin/index.php
@@ -1,31 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/php;charset=utf-8" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../images/favicon.png">
+    <title>Welcome to FlatShop</title>
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,700,500italic,100italic,100' rel='stylesheet' type='text/css'>
+    <link href="../css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/flexslider.css" type="text/css" media="screen" />
+    <link href="../css/sequence-looptheme.css" rel="stylesheet" media="all" />
+    <link href="../css/style.css" rel="stylesheet">
+    <!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/php5shiv/3.7.0/php5shiv.js"></script><script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script><![endif]-->
+</head>
 <?php
 if (!isset($_REQUEST['Username'])) {
 ?>
-    hihi
+
+    <body id="home">
+        <div class="container" style="width: 30%; margin-top: 5%;">
+            <form>
+                <div class="input-group">
+                    <img src="../images/logo.gif" alt="">
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                    <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+                </div>
+                <br>
+                <br>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                    <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                </div>
+                <br>
+                <br>
+                <div class="input-group text-center">
+                    <input type="submit" class="btn btn-default" style=";" value="Login">
+                </div>
+            </form>
+        </div>
+    </body>
 <?php
 } else {
 ?>
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta http-equiv="Content-Type" content="text/php;charset=utf-8" />
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" href="../images/favicon.png">
-        <title>Welcome to FlatShop</title>
-        <link href="../css/bootstrap.css" rel="stylesheet">
-        <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,700,500italic,100italic,100' rel='stylesheet' type='text/css'>
-        <link href="../css/font-awesome.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="../css/flexslider.css" type="text/css" media="screen" />
-        <link href="../css/sequence-looptheme.css" rel="stylesheet" media="all" />
-        <link href="../css/style.css" rel="stylesheet">
-        <!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/php5shiv/3.7.0/php5shiv.js"></script><script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script><![endif]-->
-    </head>
     <?php
     require('HeaderAdmin.php')
     ?>
-    </html>
+
 <?php
 }
 ?>
+
+</html>
