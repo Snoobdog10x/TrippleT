377b0e3daf821fd2a0780b06ae09352a99f82291
Thanh Duy Nguyen <67373941+Snoobdog10x@users.noreply.github.com>
2021-05-15 18:51:55 +0700

    Update header.php

--- Admin/header.php
+++ Admin/header.php
@@ -13,8 +13,6 @@
     <link rel="stylesheet" href="../css/flexslider.css" type="text/css" media="screen" />
     <link href="../css/sequence-looptheme.css" rel="stylesheet" media="all" />
     <link href="../css/style.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script>
         function wannadelete(pid) {
             if (confirm("Do you want to delete it?"))
