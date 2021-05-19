ec40d77c43a339d0384ec10190d4997279e29c3b
HuiT <thng.huy1202@gmail.com>
2021-05-20 00:04:08 +0700

    myorder

---
+++ myorder.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html>
+<?= require_once('lib.php') ?>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/favicon.png">
+    <title>
+        Welcome to FlatShop
+    </title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,700,500italic,100italic,100' rel='stylesheet' type='text/css'>
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js">
+</script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js">
+</script>
+<![endif]-->
+</head>
+<script>
+    function checkcard() {
+        if (document.getElementById('ccnum').value == '') {
+            document.getElementById('numcard').innerHTML = '!!!';
+            return false;
+        }
+        if (document.getElementById('expmonth').value == '') {
+            document.getElementById('exp_month').innerHTML = '!!!';
+            return false;
+        }
+        if (document.getElementById('expyear').value == '') {
+            document.getElementById('exp_year').innerHTML = '!!!';
+            return false;
+        }
+        if (document.getElementById('cvv').value == '') {
+            document.getElementById('cvv_card').innerHTML = '!!!';
+            return false;
+        }
+    }
+</script>
+
+<body id="home">
+    <div class="wrapper">
+        <?php
+        require('HeaderProduct.php') ?>
+        <div class="clearfix">
+        </div>
+        <div class="container_fullwidth">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 class="title contact-title">
+                            MY ORDER
+                        </h4>
+                        <div class="clearfix">
+                        </div>
+                    <div style="height:400px">
+                        <table style="border-collapse: collapse; width: 100%; height: 36px;" border="1">
+                            <tbody>
+                            <tr style="height: 18px; font-family:Verdana; font-size:14px;">
+                                <td style="width: 20%; height: 18px; text-align: center;">ID&nbsp;</td>
+                                <td style="width: 20%; height: 18px; text-align: center;">Purchase Date</td>
+                                <td style="width: 20%; height: 18px; text-align: center;">Product</td>
+                                <td style="width: 20%; height: 18px; text-align: center;">Price</td>
+                                <td style="width: 20%; height: 18px; text-align: center;">Order Status</td>
+                            </tr>
+                                            <?php $sql = "SELECT * FROM order WHERE USERNAME ='" . $_SESSION['Username'] . "'";
+                                                $result = $conn->query($sql);
+                                                while ($row = $result->fetch_assoc()) {
+                                                    $id = $row['OID'];
+                                                    $username = $row['USERNAME'];
+                                                    $fullname = $row['NAME'];
+                                                    $email = $row['EMAIL'];
+                                                    $address = $row['ADDRESS'];
+                                                    $phone = $row['PHONE'];
+                                                }
+                                                ?>
+                            <tr style="height: 18px;">
+                                <td style="width: 20%; height: 18px; text-align: center;" value="<?= $id?>"></td>
+                                <td style="width: 20%; height: 18px; text-align: center;">&nbsp;</td>
+                                <td style="width: 20%; height: 18px; text-align: center;">&nbsp;</td>
+                                <td style="width: 20%; height: 18px; text-align: center;">&nbsp;</td>
+                                <td style="width: 20%; height: 18px; text-align: center;">&nbsp;</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                <div class="clearfix">
+                </div>
+                <div class="our-brand">
+                        <h3 class="title">
+                            <strong>
+                                Our
+                            </strong>
+                            Brands
+                        </h3>
+                        <div class="control">
+                            <a id="prev_brand" class="prev" href="#">
+                                &lt;
+                            </a>
+                            <a id="next_brand" class="next" href="#">
+                                &gt;
+                            </a>
+                        </div>
+                        <ul id="braldLogo">
+                            <li>
+                                <ul class="brand_item">
+                                    <li>
+                                        <a href="#">
+                                            <div class="brand-logo">
+                                                <img src="images/envato.png" alt="">
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <div class="brand-logo">
+                                                <img src="images/themeforest.png" alt="">
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <div class="brand-logo">
+                                                <img src="images/photodune.png" alt="">
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <div class="brand-logo">
+                                                <img src="images/activeden.png" alt="">
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <div class="brand-logo">
+                                                <img src="images/envato.png" alt="">
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul class="brand_item">
+                                    <li>
+                                        <a href="#">
+                                            <div class="brand-logo">
+                                                <img src="images/envato.png" alt="">
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <div class="brand-logo">
+                                                <img src="images/themeforest.png" alt="">
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <div class="brand-logo">
+                                                <img src="images/photodune.png" alt="">
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <div class="brand-logo">
+                                                <img src="images/activeden.png" alt="">
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <div class="brand-logo">
+                                                <img src="images/envato.png" alt="">
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix">
+            </div>
+            <div class="footer">
+                <div class="footer-info">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="footer-logo">
+                                    <a href="#">
+                                        <img src="images/logo.png" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-6">
+                                <h4 class="title">
+                                    Contact
+                                    <strong>
+                                        Info
+                                    </strong>
+                                </h4>
+                                <p>
+                                    No. 08, Nguyen Trai, Hanoi , Vietnam
+                                </p>
+                                <p>
+                                    Call Us : (084) 1900 1008
+                                </p>
+                                <p>
+                                    Email : michael@leebros.us
+                                </p>
+                            </div>
+                            <div class="col-md-3 col-sm-6">
+                                <h4 class="title">
+                                    Customer
+                                    <strong>
+                                        Support
+                                    </strong>
+                                </h4>
+                                <ul class="support">
+                                    <li>
+                                        <a href="#">
+                                            FAQ
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Payment Option
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Booking Tips
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Infomation
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-md-3">
+                                <h4 class="title">
+                                    REQUEST Our
+                                    <strong>
+                                        Newsletter
+                                    </strong>
+                                </h4>
+                                <p>
+                                    Lorem ipsum dolor ipsum dolor.
+                                </p>
+                                <form class="newsletter">
+                                    <input type="text" name="" placeholder="Type your email....">
+                                    <input type="submit" value="SignUp" class="button">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="copyright-info">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p>
+                                    Copyright © 2012. Designed by
+                                    <a href="#">
+                                        Michael Lee
+                                    </a>
+                                    . All rights reseved
+                                </p>
+                            </div>
+                            <div class="col-md-6">
+                                <ul class="social-icon">
+                                    <li>
+                                        <a href="#" class="linkedin">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="google-plus">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="twitter">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="facebook">
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- Bootstrap core JavaScript==================================================-->
+    <script type="text/javascript" src="js/jquery-1.10.2.min.js">
+    </script>
+    <script type="text/javascript" src="js/bootstrap.min.js">
+    </script>
+    <script defer src="js/jquery.flexslider.js">
+    </script>
+    <script type="text/javascript" src="js/jquery.carouFredSel-6.2.1-packed.js">
+    </script>
+    <script type="text/javascript" src="js/script.min.js">
+    </script>
+</body>
+
+</html>
