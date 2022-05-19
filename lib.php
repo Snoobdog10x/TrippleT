e002f847e34b1259439ddd2113b0344f8a465a54
Thanh Duy Nguyen <67373941+Snoobdog10x@users.noreply.github.com>
2022-05-19 22:31:40 +0700

    Update lib.php

--- lib.php
+++ lib.php
@@ -1,7 +1,7 @@
 <?php
  $servername = "localhost";
  $username = "root";
- $password = "";
+ $password = "thanhanh";
  $dbname = "tripplet";
  $conn = new mysqli($servername, $username, $password, $dbname);
  if ($conn->connect_error) {
